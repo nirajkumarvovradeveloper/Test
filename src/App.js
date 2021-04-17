3d6a9c50485a82dd25671b1df914cfb20cce769d
nirajkumarvovradeveloper <80186270+nirajkumarvovradeveloper@users.noreply.github.com>
2021-04-17 16:49:37 -0400

    First Commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from './Components/Header';
+import Landing from './Components/Landing';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+  <div className="App">
+    <Header />
+    <Landing />
+    <Footer />
+  </div>
+  );
+}
+
+export default App;
